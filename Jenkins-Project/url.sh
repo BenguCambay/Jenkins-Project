903ca379872e4444fc89f394eaf5a10cc9762fd9
BenguCambay <123668329+BenguCambay@users.noreply.github.com>
2024-10-21 12:25:57 +0300

    Delete Jenkins-Project directory

--- Jenkins-Project/url.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Terraform'dan public IP'yi al
-PUBLIC_IP=$(terraform output -raw public_ip)
-
-# .env dosyasına yaz
-echo "PUBLIC_IP=${PUBLIC_IP}" > /home/ec2-user/Jenkins-Project/.env
